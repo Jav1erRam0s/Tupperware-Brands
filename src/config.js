beeb05dfbb42547c260eba06791cb7fe99656316
Jav1erRam0s <ramos.aranibar.javier@gmail.com>
2022-11-15 09:17:26 -0300

    Change path json-api

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const url = {
   tupperware:
-    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware/main/productos/tupperware.json",
+    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware.Brands/main/productos/tupperware.json",
   homeware:
-    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware/main/productos/homeware.json",
+    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware.Brands/main/productos/homeware.json",
   fullercosmetics:
-    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware/main/productos/fullercosmetics.json",
+    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware.Brands/main/productos/fullercosmetics.json",
   promociones:
-    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware/main/promociones.json",
+    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware.Brands/main/promociones.json",
   folletos:
-    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware/main/folletos.json",
+    "https://raw.githubusercontent.com/Jav1erRam0s/JSON-API-Tupperware.Brands/main/folletos.json",
 };
 
 export default url;
